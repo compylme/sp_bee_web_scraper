5d7b8542ac060a2d571b280c8f2f4a72965e5520
compylme <adthompson10@gmail.com>
2023-09-14 15:49:10 +0100

    db integration

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from helpers.functions import insert_letters_db, remove_spaces
+from google.cloud import firestore
 import datetime
 import pytz
 import chromedriver_binary
+import uuid
 
 app = Flask(__name__)
+db = firestore.Client(project = "spelling-bee-web-scraper")
+
+daily_letters = "spellingbee letters"
+this_date = datetime.datetime.now()
+
+doc_id = str(uuid.uuid4())
+
 
 @app.route('/')
 def scraper():
@@ -33,10 +43,14 @@ def scraper():
             mandatory_letter = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR,"article.css-1vxca1d.e1lmdhsb0 section.meteredContent.css-1r7ky0e:nth-child(4) section.interactive-content.interactive-size-medium.css-14l1964 div.css-17ih8de.interactive-body div:nth-child(4) p.content:nth-child(2) > span:nth-child(1)")))
             anagram_letters_value = anagram_letters.get_attribute("textContent")
             mandatory_letter_value = mandatory_letter.get_attribute("textContent")
-            
+
+            stripped_anagram = remove_spaces(anagram_letters_value)
+            stripped_mandatory = remove_spaces(mandatory_letter_value)
+
             return {
-                "anagram_letters": anagram_letters_value,
-                "mandatory_letter": mandatory_letter_value
+                "anagram_letters": stripped_anagram,
+                "mandatory_letter": stripped_mandatory,
+                
             }
         except Exception as e:
             print("An error occured while scraping:", str (e))
@@ -45,6 +59,17 @@ def scraper():
     except Exception as e:
         print("An error occureed while initializing the WebDriver:", str(e))
         return None
+
+letter_dictionary = scraper()
+
+anagram,mandatory = letter_dictionary.values()
+
+payload_data = {"date": str(this_date) , "letters": anagram, "mandatory": mandatory}
+print(payload_data)
     
 if __name__ == '__main__':
     app.run()
+    try:
+        insert_letters_db(daily_letters, payload_data, doc_id)
+    except:
+        print("An exception occured")
