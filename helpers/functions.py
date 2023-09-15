6744d6fdb83bef095dc973722ee45badfe38ebc4
compylme <adthompson10@gmail.com>
2023-09-15 22:08:11 +0100

    adding logs to funcs

--- helpers/functions.py
+++ helpers/functions.py
@@ -1,8 +1,19 @@
-from google.cloud import firestore
+from firebase_admin import firestore, credentials
+import firebase_admin
 
-db = firestore.Client(project = "spelling-bee-web-scraper")
-def insert_letters_db(collection, payload, document_id):
-    return db.collection(collection).document(document_id).set(payload)
+cred = credentials.Certificate('service-account-key.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+def insert_letters_db(collection_name, payload, document_id):
+    try:
+        db = firestore.Client()
+        collection = db.collection(collection_name)
+        document_ref = collection.document(document_id)
+        document_ref.set(payload)
+        print("Data inserted successfully into Firestore")
+    except Exception as e:
+        print("An error occurred while inserting data into Firestore:", str(e))
 
 
 def remove_spaces(string):
